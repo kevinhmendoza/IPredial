688f98f6ed611ac5b973aff1f64ffabeacb3b9a5
inganibalguerra <IngAnibalJoseGuerra@hotmail.com>
2020-09-21 22:51:23 -0500

    se visualizan botones para ir al login y/o el portal.

--- Application.Frontend/src/app/routes/portal.routing.js
+++ Application.Frontend/src/app/routes/portal.routing.js
@@ -2,7 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Portal = void 0;
 var estado_cuenta_component_1 = require("../pages/portal/estado-cuenta/estado-cuenta.component");
+var auth_layout_component_1 = require("../layouts/auth-layout/auth-layout.component");
 exports.Portal = {
-    path: 'Portal/Estado/Cuenta', component: estado_cuenta_component_1.EstadoCuentaComponent
+    path: 'Portal', component: auth_layout_component_1.AuthLayoutComponent,
+    children: [
+        {
+            path: 'Estado/Cuenta', component: estado_cuenta_component_1.EstadoCuentaComponent
+        }
+    ]
 };
 //# sourceMappingURL=portal.routing.js.map
--- Application.Frontend/src/app/routes/portal.routing.ts
+++ Application.Frontend/src/app/routes/portal.routing.ts
@@ -1,5 +1,11 @@
 import { EstadoCuentaComponent } from '../pages/portal/estado-cuenta/estado-cuenta.component';
+import { AuthLayoutComponent } from '../layouts/auth-layout/auth-layout.component';
 
 export const Portal: any = {
-  path: 'Portal/Estado/Cuenta', component: EstadoCuentaComponent
+  path: 'Portal', component: AuthLayoutComponent,
+  children: [
+    {
+      path: 'Estado/Cuenta', component: EstadoCuentaComponent
+    }
+  ]
 };
